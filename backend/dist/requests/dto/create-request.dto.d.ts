7a7a3f3c3c743f4033f22cd6c082e080ce383c69
Gregorio D'Angelo <gregoriodangelo@gmail.com>
2026-02-14 15:58:48 -0300

    wip

--- backend/dist/requests/dto/create-request.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateRequestDto {
-    make: string;
-    model: string;
-    year: number;
-    color?: string;
-    partName: string;
-    description?: string;
-    photoUrl?: string;
-}
--- backend/dist/requests/dto/update-request.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateRequestDto } from './create-request.dto';
-declare const UpdateRequestDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateRequestDto>>;
-export declare class UpdateRequestDto extends UpdateRequestDto_base {
-}
-export {};
